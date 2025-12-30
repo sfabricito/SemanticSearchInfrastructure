61188340f49618e0e2acba9c75bcbb6a82cc443c
Fabricio Solis <sfabricito@gmail.com>
2025-12-30 13:56:26 -0600

    fix: ingest env variables

--- charts/install.sh
+++ charts/install.sh
@@ -7,9 +7,9 @@ DATABASES_CHART="$SCRIPT_DIR/databases"
 BATCH_CHART="$SCRIPT_DIR/batch"
 APPLICATION_CHART="$SCRIPT_DIR/application"
 
-if ! helm repo list | tail -n +2 | awk '{print $1}' | grep -qx spark-operator; then
-  helm repo add spark-operator https://kubeflow.github.io/spark-operator
-fi
+# if ! helm repo list | tail -n +2 | awk '{print $1}' | grep -qx spark-operator; then
+#   helm repo add spark-operator https://kubeflow.github.io/spark-operator
+# fi
 
 # if ! helm repo list | tail -n +2 | awk '{print $1}' | grep -qx qdrant; then
 #   helm repo add qdrant https://qdrant.github.io/qdrant-helm
@@ -22,6 +22,6 @@ helm repo update
 helm dependency update "$DATABASES_CHART"
 helm dependency update "$BATCH_CHART"
 
-# helm upgrade --install databases "$DATABASES_CHART"
+helm upgrade --install databases "$DATABASES_CHART"
 helm upgrade --install batch "$BATCH_CHART"
-# helm upgrade --install application "$APPLICATION_CHART"
+helm upgrade --install application "$APPLICATION_CHART"
